3dc825dbe0e8019a7dee3057fc77ab09230d6cac
iamunique014 <alungileunique@gmail.com>
2025-02-11 12:40:34 +0200

    Rolebased auth

---
+++ TripCoordination.UI/Constants/Contants.cs
@@ -0,0 +1,14 @@
+﻿namespace TripCoordination.Constants
+{
+    public enum Roles
+    {
+        Admin,
+        User,
+        Student,
+        Organizer
+    }
+    public class Contants
+    {
+
+    }
+}
