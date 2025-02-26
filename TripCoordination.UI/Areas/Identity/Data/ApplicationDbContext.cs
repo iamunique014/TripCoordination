13b01fee4630cbe28173d8e6bf5b99e4cd255aa0
iamunique014 <alungileunique@gmail.com>
2025-02-26 16:31:39 +0200

    Added Role selection during user Registration

--- TripCoordination.UI/Areas/Identity/Data/ApplicationDbContext.cs
+++ TripCoordination.UI/Areas/Identity/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TripCoordination.Areas.Identity.Data;
 
 namespace TripCoordination.Data.Models.Data;
 
