8691e3d6792fe4c6fc9224e68e1995497734739d
iamunique014 <alungileunique@gmail.com>
2025-05-10 12:02:30 +0200

    Added TripID property

--- TripCoordination.Common/ViewModel/TripListingViewModel.cs
+++ TripCoordination.Common/ViewModel/TripListingViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace TripCoordination.Common.ViewModel
 {
     public class TripListingViewModel
     {
+        public int TripID { get; set; }
         [Required(ErrorMessage = "Please select a destination")]
         public int DestinationID { get; set; }
         [Required(ErrorMessage = "Please Enter a pickUpPoint eg. Res Address")]
@@ -13,6 +15,6 @@ namespace TripCoordination.Common.ViewModel
         public string? Surname { get; set; }
         public string? DestinationName { get; set; }
         public int Seats { get; set; }
-
+              
     }
 }
