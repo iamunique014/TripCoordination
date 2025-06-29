54394d0951006ef4c58106ffd368c1681432b87d
iamunique014 <alungileunique@gmail.com>
2025-06-30 00:59:58 +0200

    Added TripFlatRowUI

---
+++ TripCoordination.UI/ViewModel/TripFlatRowUI.cs
@@ -0,0 +1,15 @@
+﻿namespace TripCoordination.ViewModel
+{
+    public class TripFlatRowUI
+    {
+        public int TripID { get; set; }
+        public string CreatorName { get; set; } // Organizer first name
+        public string CreatorSurname { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public bool IsFull { get; set; }
+        public int Seats { get; set; }
+        public int? DestinationID { get; set; }
+        public string DestinationName { get; set; }
+        public int DestinationTownID { get; set; }
+    }
+}
