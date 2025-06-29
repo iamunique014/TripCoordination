43ab6e2d65e6bf938e764df36bdc4ac1e922681f
iamunique014 <alungileunique@gmail.com>
2025-06-29 21:32:58 +0200

    Added a tripViewModel

---
+++ TripCoordination.Common/ViewModel/TripViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Common.ViewModel
+{
+    public class TripViewModel
+    {
+        public int TripID { get; set; }
+        public int UserID { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public int Seats { get; set; }
+        public string CreatorName { get; set; }
+        public string CreatorSurname { get; set; }
+        public int IsFull { get; set; }
+    }
+}
