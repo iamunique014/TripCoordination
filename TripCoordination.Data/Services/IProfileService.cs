5d7712553b910e83c2a24c4017ba25772e0b7c25
iamunique014 <alungileunique@gmail.com>
2025-07-01 02:30:20 +0200

    Added and implemted profile Service

---
+++ TripCoordination.Data/Services/IProfileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Data.Services
+{
+    // IProfileService.cs
+    public interface IProfileService
+    {
+        Task<bool> HasProfileAsync(string userID);
+    }
+}
