4e4d52c5ff7fd9efcf132508b8ae7f2b6e5a2dd1
iamunique014 <alungileunique@gmail.com>
2025-07-02 17:26:14 +0200

    Fix: Ensured all validations work

---
+++ TripCoordination.Common/MinSelectedItemsAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripCoordination.Common
+{
+    public class MinSelectedItemsAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            var list = value as List<int>;
+            return list != null && list.Any();
+        }
+    }
+}
