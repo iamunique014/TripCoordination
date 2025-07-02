ae58a0aba12880919a0871e2960a1a24a2f7a4c3
iamunique014 <alungileunique@gmail.com>
2025-07-02 17:26:14 +0200

    Fix: Ensured all validations work

--- TripCoordination.Common/ViewModel/CreateTripViewModel.cs
+++ TripCoordination.Common/ViewModel/CreateTripViewModel.cs
@@ -9,16 +9,16 @@ namespace TripCoordination.Common.ViewModel
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddTHH:mm}", ApplyFormatInEditMode = true)]
         public DateTime DepartureDate { get; set; }
 
-        [Required(ErrorMessage = "Please select trip destination.")]
-        public int DestinationID { get; set; }
+        //[Required(ErrorMessage = "Please select trip destination.")]
+        //public int? DestinationID { get; set; }
         [Required(ErrorMessage = "Please enter number of seats.")]
-        public int Seats { get; set; }
+        public int? Seats { get; set; }
 
         public bool IsFull { get; set; }
 
         // Multi-select value: list of selected town IDs
-        [Required(ErrorMessage = "Please select at least one drop-off location.")]
-        public List<int> SelectedTownIds { get; set; } = new List<int>();
+        [MinSelectedItems(ErrorMessage = "Please select at least one drop-off location.")]
+        public List<int>? SelectedTownIds { get; set; } = new List<int>();
     }
 }
 
