76456380696ef6ad7d30abf387598fbbeddd28e9
iamunique014 <alungileunique@gmail.com>
2025-07-04 05:45:37 +0200

    Added ViewParticipants controller method

---
+++ TripCoordination.UI/Controllers/TripParticipantController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TripCoordination.Data.Repository;
+
+namespace TripCoordination.Controllers
+{
+    public class TripParticipantController : Controller
+    {
+        private readonly ITripParticipantRepository _tripParticipantRepository;
+
+        public TripParticipantController(ITripParticipantRepository tripParticipantRepository)
+        {
+            _tripParticipantRepository = tripParticipantRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewParticipants(int tripID)
+        {
+            ViewData["ShowSidebar"] = true;
+
+            var tripParticipants = await _tripParticipantRepository.GetParticipantsByTripIDAsync(tripID);
+
+            return View(tripParticipants);
+        }
+    }
+}
