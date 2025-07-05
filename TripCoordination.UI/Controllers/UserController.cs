2aed49c7416820cc5a8ee2c9e155a6a6abd067af
iamunique014 <alungileunique@gmail.com>
2025-07-05 17:03:48 +0200

    Feature:Added UserController and viewUsers method

---
+++ TripCoordination.UI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TripCoordination.Data.Repository;
+
+namespace TripCoordination.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewUsers()
+        {
+            ViewData["ShowSidebar"] = true;
+            var users = await _userRepository.GetAllAsync();
+            return View(users);
+        }
+    }
+}
