5386ef47d9c0d56b50272022445e8e28f88003a2
iamunique014 <alungileunique@gmail.com>
2025-07-06 17:30:21 +0200

    Feature: Added MyTripFlatRow class

--- TripCoordination.UI/Controllers/StudentController.cs
+++ TripCoordination.UI/Controllers/StudentController.cs
@@ -1,9 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TripCoordination.Data.Repository;
 
 namespace TripCoordination.Controllers
 {
     public class StudentController : Controller
     {
+        //private readonly ILogger<AdminController> _logger;
+
+        private readonly ITownRepository _townRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IRoleRepository _roleRepository;
+        private readonly ITripRepository _tripRepository;
+        private readonly IProfileRepository _profileRepository;
+        private readonly IUserRoleRepository _UserRoleRepository;
+        private readonly IResidenceRepository _residenceRepository;
+        private readonly ITripParticipantRepository _tripParticipantRepository;
+        private readonly ITripDestinationTownRepository _tripDestinationTownRepository;
+
+
+        public StudentController(/*Logger<AdminController> logger*/ ITownRepository townRepository, IResidenceRepository residenceRepository, IUserRepository userRepository, IProfileRepository profileRepository, IUserRoleRepository userRoleRepository, IRoleRepository roleRepository, ITripRepository tripRepository, ITripParticipantRepository tripParticipantRepository, ITripDestinationTownRepository tripDestinationTownRepository)
+        {
+            //_logger = logger;
+            _townRepository = townRepository;
+            _residenceRepository = residenceRepository;
+            _userRepository = userRepository;
+            _profileRepository = profileRepository;
+            _UserRoleRepository = userRoleRepository;
+            _roleRepository = roleRepository;
+            _tripRepository = tripRepository;
+            _tripParticipantRepository = tripParticipantRepository;
+            _tripDestinationTownRepository = tripDestinationTownRepository;
+        }
         public IActionResult Index()
         {
             return View();
