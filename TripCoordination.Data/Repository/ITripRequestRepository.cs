91d6913cf220e6261f76d59a0fb72f3d417aed03
iamunique014 <alungileunique@gmail.com>
2025-07-12 00:33:27 +0200

    Add: New Interface ITripRequestRepository

---
+++ TripCoordination.Data/Repository/ITripRequestRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Data.Models.Domain;
+
+namespace TripCoordination.Data.Repository
+{
+    public interface ITripRequestRepository
+    {
+        Task<IEnumerable<TripRequest>> GetAllAsync();
+        Task<TripRequest?> GetByIdAsync(int id);
+        Task AddAsync(TripRequest request);
+        Task DeleteAsync(int id);
+        Task ApproveAsync(int id); 
+    }
+}
