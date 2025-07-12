2ba85d44414928110b8724d5013a4463762a6af8
iamunique014 <alungileunique@gmail.com>
2025-07-12 16:12:26 +0200

    Added and implemented IRouteRequestRepository

---
+++ TripCoordination.Data/Repository/IRouteRequestRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Data.Models.Domain;
+
+namespace TripCoordination.Data.Repository
+{
+    public interface IRouteRequestRepository
+    {
+        Task<bool> AddAsync(RouteRequest request);
+        Task<bool> DeleteAsync(int routeRequestID);
+        Task<IEnumerable<RouteRequest>> GetAllAsync();
+
+        Task<IEnumerable<RouteRequest>> GetAllUserRouteRequestAsync(string userID);
+        Task<RouteRequest?> GetByIdAsync(int routeRequestID);
+        Task<bool> ApproveAsync(int routeRequestID);
+    }
+}
