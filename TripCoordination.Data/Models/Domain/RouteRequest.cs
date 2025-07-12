2b66e36644ca4fdacab7c1b12d43541dd9cae718
iamunique014 <alungileunique@gmail.com>
2025-07-12 15:30:13 +0200

    Add: Created a RouteRequest Model and  VIewModel

---
+++ TripCoordination.Data/Models/Domain/RouteRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Data.Models.Domain
+{
+    public class RouteRequest
+    {
+        public int RouteRequestID { get; set; }
+        public string FromLocation { get; set; }
+        public string ToLocation { get; set; }
+        public string? Reason { get; set; }
+        public string UserID { get; set; }
+        public DateTime RequestedAt { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
