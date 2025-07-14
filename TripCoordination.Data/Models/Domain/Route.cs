22d1f9930ebf6812ed7234278aba003eb5722604
iamunique014 <alungileunique@gmail.com>
2025-07-14 10:12:43 +0200

    Feature: Added and implemented routes in repositories

---
+++ TripCoordination.Data/Models/Domain/Route.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Data.Models.Domain
+{
+    public class Route
+    {
+        public int RouteID { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public string FromLocation { get; set; } = string.Empty;
+
+        public string ToLocation { get; set; } = string.Empty;
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ TripCoordination.Data/Repository/IRouteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Data.Models.Domain;
+
+namespace TripCoordination.Data.Repository
+{
+    public interface IRouteRepository
+    {
+        Task<IEnumerable<Route>> GetAllAsync();
+        Task<Route?> GetByIDAsync(int routeID);
+        Task<bool> CreateAsync(Route route);
+        Task<bool> UpdateAsync(Route route);
+        Task<bool> SoftDeleteAsync(int routeID);
+    }
+}
