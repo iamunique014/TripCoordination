2dfca68f545908473fd2108457edc975b361deec
iamunique014 <alungileunique@gmail.com>
2025-07-23 21:45:53 +0200

    UI:Datatables clean up

--- TripCoordination.UI/wwwroot/js/site.js
+++ TripCoordination.UI/wwwroot/js/site.js
@@ -54,6 +54,8 @@ function setupDataTable(tableSelector) {
 
             ],
             responsive: true,
+            scrollX: true,
+            autoWidth: false,
             pageLength: 5,
             lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]]
         });
