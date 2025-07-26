d0a73d9344092dbc2e00a4a6c330561292ad897e
iamunique014 <alungileunique@gmail.com>
2025-07-26 09:53:29 +0200

    Add:Added a createProfileViewModel

---
+++ TripCoordination.Common/ViewModel/CreateProfileViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TripCoordination.Common.ViewModel
+{
+    public class CreateProfileViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int ResidenceID { get; set; }
+        public string? UserID { get; set; }
+
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? AvailableResidences { get; set; }
+    }
+}
