6e49313dabc0f1355fae65be2f1154e6341f7ad9
iamunique014 <alungileunique@gmail.com>
2025-07-29 16:27:04 +0200

    Feat:Added and Implemented StudentDashBoardRepo

---
+++ TripCoordination.Data/Repository/IStudentDashboardRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Common.ViewModel;
+
+namespace TripCoordination.Data.Repository
+{
+    public interface IStudentDashboardRepository
+    {
+        Task<UpcomingTripViewModel?> GetNextUpcomingTrip(string userID);
+        Task<IEnumerable<TripRequestViewModel>> GetRecentTripRequests(string userID);
+    }
+}
---
+++ TripCoordination.Data/Repository/StudentDashboardRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Common.ViewModel;
+using TripCoordination.Data.DataAccess;
+
+namespace TripCoordination.Data.Repository
+{
+    public class StudentDashboardRepository: IStudentDashboardRepository
+    {
+        private readonly ISqlDataAccess _db;
+
+        public StudentDashboardRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public async Task<UpcomingTripViewModel?> GetNextUpcomingTrip(string userID)
+        {
+            var results = await _db.GetData<UpcomingTripViewModel, dynamic>(
+                "sp_GetNextUpcomingTrip",
+                new { UserID = userID }
+            );
+
+            return results.FirstOrDefault();
+        }
+        public async Task<IEnumerable<TripRequestSummaryViewModel>> GetRecentTripRequests(string userId)
+        {
+            return await _db.GetData<TripRequestSummaryViewModel, dynamic>(
+                "sp_GetRecentTripRequests",
+                new { UserID = userId }
+            );
+        }
+    }
+}
