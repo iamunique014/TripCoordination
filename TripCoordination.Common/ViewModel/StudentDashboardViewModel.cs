4ff61c65aac179d6e9164c34a2d3a36e63326206
iamunique014 <alungileunique@gmail.com>
2025-07-29 16:09:00 +0200

    Add:StudentDashboard ViewModel

---
+++ TripCoordination.Common/ViewModel/StudentDashboardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Common.ViewModel
+{
+    public class StudentDashboardViewModel
+    {
+        public UpcomingTripViewModel? UpcomingTrip { get; set; }
+
+        // Quick stats
+        public int TotalTripsJoined { get; set; }
+        public int TotalRequestsMade { get; set; }
+        public int TripsJoinedThisMonth { get; set; }
+
+        public List<TripRequestSummaryViewModel> RecentTripRequests { get; set; } = new();
+    }
+}
