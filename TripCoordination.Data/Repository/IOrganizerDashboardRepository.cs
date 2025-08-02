20c291966b2d37f64eba0780e05cae3aa11a9a0a
iamunique014 <alungileunique@gmail.com>
2025-08-02 16:08:13 +0200

    Feat:Added and implemented the IOrganizerDashboardRepository

---
+++ TripCoordination.Data/Repository/IOrganizerDashboardRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripCoordination.Common.ViewModel;
+
+namespace TripCoordination.Data.Repository
+{
+    public interface IOrganizerDashboardRepository
+    {
+        Task<UpcomingTripViewModel?> GetUpcomingTrip(string userID);
+        Task<OrganizerTripStatsViewModel?> GetOrganizerTripStats(string userID);
+        Task<IEnumerable<TripRequestSummaryViewModel>>? GetRecentTripRequests();
+    }
+}
