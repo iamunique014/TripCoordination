297e00efb1b1c1ff33d65546b23135d870ab7f6b
iamunique014 <alungileunique@gmail.com>
2025-08-02 13:10:04 +0200

    ADD:TripCreatorControllerTests

---
+++ TripCoordination.Tests/TripCreatorControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Tests
+{
+    public class TripCreatorControllerTests
+    {
+        // Placeholder for future tests for TripCreatorController
+        // This class will contain unit tests for the TripCreatorController methods
+        // such as Index, CreateTrip, OrganizerDashboard, and MyTrips.
+        // Each test will mock dependencies and verify the expected behavior of the controller actions.
+
+
+    }
+}
