7bebae1b1f351d80d3d608e8f0340ee10f53f0a1
iamunique014 <alungileunique@gmail.com>
2025-08-04 05:57:47 +0200

    Add: Models for Admin Dashboard

---
+++ TripCoordination.Common/ViewModel/RecentActivityViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Common.ViewModel
+{
+    public class RecentActivityViewModel
+    {
+        public string ActivityType { get; set; }
+        public string Description { get; set; }
+        public DateTime ActivityDate { get; set; }
+    }
+
+}
