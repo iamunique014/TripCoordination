e3298f1ba87439ec4d911e856636f0c0bfef981b
iamunique014 <alungileunique@gmail.com>
2025-09-03 06:53:00 +0200

    Add: TripStop Model

---
+++ TripCoordination.Common/ViewModel/TripStop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripCoordination.Common.ViewModel
+{
+    public class TripStop
+    {
+        public int TripID { get; set; }
+        public int TownID { get; set; }
+        public string? Price { get; set; }
+    }
+}
